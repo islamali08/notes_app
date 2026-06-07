8949aac9ab164ccfc184aded86ece46a580386d5
islamali08 <islam084@hotmail.com>
2026-06-07 16:42:54 +0300

    change to dark them and add home view

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(),
+    );
   }
 }
